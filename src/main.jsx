138b0613190ea096cf6e6efe4594710f2d013ddc
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2024-05-23 23:30:41 +0700

    feat: removing coma

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
         <ToastContainer />
         <App />
-      </QueryClientProvider>,
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
