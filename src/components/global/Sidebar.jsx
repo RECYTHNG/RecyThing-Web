acfaec45551aec37447f760e9b7c92df2162be52
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2024-05-25 13:26:17 +0700

    feat: update layout and create manage report page

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   const location = useLocation();
 
   return (
-    <nav className='h-screen flex flex-col px-[14px] pt-[37px] pb-[27px] text-[#737791] overflow-y-auto sidebar overflow-x-hidden'>
+    <nav className='h-screen min-w-[290px] flex flex-col px-[14px] pt-[37px] pb-[27px] text-[#737791] overflow-y-auto'>
       <img src={logo} alt="logo" className='text-center h-[34px]'/>
       <div className='flex flex-col gap-6 mt-11 sub-m'>
         {nav_item.map(nav => {
@@ -25,7 +25,7 @@ const Sidebar = () => {
           );
         })}
       </div>
-      <div className='flex-1 flex items-end justify-start px-7 min-h-13'>
+      <div className='flex-1 flex items-end justify-start px-7 min-h-12'>
         <div className='flex items-center gap-4'>
           <MdOutlineLogout />
           Keluar
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -4,13 +4,12 @@ import { Outlet } from 'react-router-dom'
 
 const DashboardLayout = () => {
   return (
-    <div className='h-screen flex text-black'>
+    <div className='h-screen w-screen flex text-black'>
       <Sidebar />
-      <div className='flex flex-col flex-1 h-screen'>
-        <div className='w-full bg-W px-10 py-5 flex justify-end items-center bg-primary-500 text-white'>
+      <div className='flex flex-col flex-1 w-full h-screen overflow-x-auto'>
+        <div className='w-full px-10 py-5 flex justify-end items-center bg-primary-500 text-white'>
           <div className='flex items-center gap-4 text-lg'>
             <div className='h-10 w-10 bg-gray-400 rounded-full'></div>
-            {/* <IoMdNotificationsOutline className='text-3xl' /> */}
           </div>
         </div>
         <main className='overflow-y-auto flex-1 bg-tertiary text-[#444A6D]'>
