3d8964cb981bf4aeacacd718f329236b5824f5c1
mackeyaddict <naufalyusuf90@gmail.com>
2024-05-27 21:09:36 +0700

    feat: :art: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import { Dashboard, ManageContent, ManageReport } from './pages/admin'
-import Playground from './pages/Playground';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import DashboardLayout from './layouts/DashboardLayout';
-import AddContent from './pages/admin/ManageContent/AddContent';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Dashboard, ManageContent, ManageReport } from "./pages/admin";
+import Playground from "./pages/Playground";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import DashboardLayout from "./layouts/DashboardLayout";
+import AddContent from "./pages/admin/ManageContent/AddContent";
+import EditContent from "./pages/admin/ManageContent/EditContent";
 
 function App() {
   return (
@@ -13,15 +14,16 @@ function App() {
       <ToastContainer />
       <Routes>
         <Route element={<DashboardLayout />}>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/report' element={<ManageReport />} />
-          <Route path='/content' element={<ManageContent/>} />
-          <Route path='/content/add' element={<AddContent/>} />
-          <Route path='/playground' element={<Playground />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/report" element={<ManageReport />} />
+          <Route path="/content" element={<ManageContent />} />
+          <Route path="/content/add" element={<AddContent />} />
+          <Route path="/content/edit/:id" element={<EditContent />} />
+          <Route path="/playground" element={<Playground />} />
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
