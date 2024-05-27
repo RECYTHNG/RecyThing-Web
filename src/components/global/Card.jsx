07ec365e0dd5699083f8974112c0458a1f6c1b4d
mackeyaddict <naufalyusuf90@gmail.com>
2024-05-27 21:10:34 +0700

    feat: :art: add onCllick props

--- src/components/global/Card.jsx
+++ src/components/global/Card.jsx
@@ -1,6 +1,6 @@
-export default function Card({ image, title }) {
+export default function Card({ image, title, onClick }) {
   return (
-    <div className="flex flex-col gap-2 max-w-[220px] h-[300px] rounded-2xl shadow-md">
+    <div className="flex flex-col gap-2 max-w-[220px] h-[300px] rounded-2xl shadow-md" onClick={onClick}>
       <div className="h-[212px] w-full overflow-hidden rounded-t-2xl">
         <img src={image} alt={title} className="h-full w-full object-cover hover:scale-110 transition-transform duration-300" />
       </div>
