a5fe5aa9a6cc52ed1c7b4a89f8f908b7aa6c8332
mackeyaddict <naufalyusuf90@gmail.com>
2024-06-02 02:04:36 +0700

    feat: :art: add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import './CustomAnt.css'
 import { Routes, Route } from 'react-router-dom'
-import { Dashboard, ManageAdmin, ManageContent, ManageReport, MissionList, ManageUserAchievements } from './pages/admin'
+import { Dashboard, ManageAdmin, ManageContent, ManageReport, MissionList, ManageUserAchievements, ManageApprovalTask } from './pages/admin'
 import Playground from './pages/Playground';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import DashboardLayout from './layouts/DashboardLayout';
-import AddContent from './pages/admin/ManageContent/AddContent';
-import EditContent from "./pages/admin/ManageContent/EditContent";
+import AddContentArticle from './pages/admin/ManageContent/AddContentArticle';
+import EditContentArticle from "./pages/admin/ManageContent/EditContentArticle";
+import AddContentVideo from './pages/admin/ManageContent/AddContentVideo';
+import EditContentVideo from './pages/admin/ManageContent/EditContentVideo';
 
 function App() {
   return (
@@ -18,11 +20,13 @@ function App() {
           <Route path='/' element={<Dashboard />} />
           <Route path='/report' element={<ManageReport />} />
           <Route path='/content' element={<ManageContent/>} />
-          <Route path='/content/add' element={<AddContent/>} />
-          <Route path="/content/edit/:id" element={<EditContent />} />
+          <Route path='/content/add-article' element={<AddContentArticle/>} />
+          <Route path="/content/edit-article/:id" element={<EditContentArticle />} />
+          <Route path='/content/add-video' element={<AddContentVideo/>} />
+          <Route path='/content/edit-video/:id' element={<EditContentVideo/>} />
           <Route path='/achievement' element={<ManageUserAchievements/>}/>
           <Route path='/mission/list' element={<MissionList />} />
-          <Route path='/mission/approval' element={<MissionList />} />
+          <Route path='/mission/approval' element={<ManageApprovalTask/>} />
           <Route path="/admin" element={<ManageAdmin />} />
         </Route>
       </Routes>
