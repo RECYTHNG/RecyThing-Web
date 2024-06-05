419258cd2d95cdde12d73c5ef51e5b6f1cdc21f3
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2024-06-06 00:50:31 +0700

    config: change configuration for api

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const APIInstance = axios.create({
-  baseURL: 'https://662394b63e17a3ac846f9b84.mockapi.io/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL, 
 });
 
 export default APIInstance
