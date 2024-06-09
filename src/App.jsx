95c997df5d9845e3b8e0a2d5df72b20b0b71c6ec
sagistarz <novialestarin@gmail.com>
2024-06-09 20:00:07 +0700

    feat: :art: add route manage user detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import './App.css';
-import './CustomAnt.css';
-import { Routes, Route, useLocation, Outlet } from 'react-router-dom';
-import { Dashboard, ManageAdmin, ManageContent, ManageReport, MissionList, ManageUserAchievements, ManageApprovalTask, ManageOpenAI, LoginPage } from './pages/admin';
-import Playground from './pages/Playground';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import DashboardLayout from './layouts/DashboardLayout';
-import AddContentArticle from './pages/admin/ManageContent/AddContentArticle';
-import EditContentArticle from './pages/admin/ManageContent/EditContentArticle';
-import AddContentVideo from './pages/admin/ManageContent/AddContentVideo';
-import EditContentVideo from './pages/admin/ManageContent/EditContentVideo';
-import LandingPage from './pages/landing/LandingPage';
-import NotFound from './pages/404';
-import { useEffect } from 'react';
-import PrivateRoute from './Routes/PrivateRoute';
+import "./App.css";
+import "./CustomAnt.css";
+import { Routes, Route, useLocation, Outlet } from "react-router-dom";
+import { Dashboard, ManageAdmin, ManageContent, ManageReport, MissionList, ManageUserAchievements, ManageApprovalTask, ManageOpenAI, LoginPage } from "./pages/admin";
+import Playground from "./pages/Playground";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import DashboardLayout from "./layouts/DashboardLayout";
+import AddContentArticle from "./pages/admin/ManageContent/AddContentArticle";
+import EditContentArticle from "./pages/admin/ManageContent/EditContentArticle";
+import AddContentVideo from "./pages/admin/ManageContent/AddContentVideo";
+import EditContentVideo from "./pages/admin/ManageContent/EditContentVideo";
+import LandingPage from "./pages/landing/LandingPage";
+import NotFound from "./pages/404";
+import { useEffect } from "react";
+import PrivateRoute from "./Routes/PrivateRoute";
+import ManageUserDetail from "./pages/admin/ManageUserDetail";
 
 function App() {
-
   function ScrollToTop() {
     const { pathname } = useLocation();
 
@@ -49,6 +49,7 @@ function App() {
             <Route path="/admin/mission/approval" element={<ManageApprovalTask />} />
             <Route path="/admin/admin" element={<ManageAdmin />} />
             <Route path="/admin/openai" element={<ManageOpenAI />} />
+            <Route path="/admin/user" element={<ManageUserDetail />} />
           </Route>
         </Route>
         {/* </Route> */}
